b8cb5c961671b416daf950608c4d51714a0d1c95
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2023-01-04 18:51:38 +0100

    remaniement arborescence

--- code/sono_light_reservation_angular/src/app/app.component.spec.ts
+++ code/sono_light_reservation_angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularcliproject'`, () => {
+  it(`should have as title 'sono_light_reservation_angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularcliproject');
+    expect(app.title).toEqual('sono_light_reservation_angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularcliproject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('sono_light_reservation_angular app is running!');
   });
 });
