2acb0c2cb40a7409fe82d0927d7f031c1787e97a
MarieJm <jomain.marie@gmail.com>
2023-01-04 16:42:36 +0100

    initialisation partie back

---
+++ code/back-java/src/main/java/backjava/backjava/BackJavaApplication.java
@@ -0,0 +1,13 @@
+package backjava.backjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackJavaApplication.class, args);
+	}
+
+}
