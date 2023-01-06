1d0c8fdfd6bc4af5bf4a929b4912b14283db8bc7
Marie Jmn <72447789+Syneaa@users.noreply.github.com>
2023-01-06 14:14:23 +0100

    Initialisation back (#16)
    * initialisation partie back
    
    * test push back

---
+++ code/back-java/src/test/java/backjava/backjava/BackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package backjava.backjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
