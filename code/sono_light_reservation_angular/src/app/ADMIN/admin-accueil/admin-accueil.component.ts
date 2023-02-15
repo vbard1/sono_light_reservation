927c0c7ae0504ed56ef64c9cbff52ad2c7c9f5a2
Victor BARDIN <victor.bardin@ipilyon.net>
2023-02-15 12:01:43 +0100

    Màj boot projet et bases ecran compte (#32)
    * installer boostrap et Angular materials
    
    * Delete Diagramme sans nom - diagrams.net – Brave 2022-12-02 00-23-57.mp4
    
    * retrait du placeholder (#21)
    
    retrait du placeholder (page par défaut dans angular)
    
    * Bases du front et routes basiques
    
    * ecran principale admin sans style
    
    + creation des composants de base (vides) de l'écran admin
    
    * écran compte base fornt cote admin
    
    * Améliorations fornt admin-comptes
    
    * tests taille front comptes
    
    * persistance focus onglet ADMIN general
    
    * test
    
    * test
    
    * Update admin-accueil.component.ts (#31)
    
    ---------
    
    Co-authored-by: kouaouci <karimakouaouci2803@gmail.com>

--- code/sono_light_reservation_angular/src/app/ADMIN/admin-accueil/admin-accueil.component.ts
+++ code/sono_light_reservation_angular/src/app/ADMIN/admin-accueil/admin-accueil.component.ts
@@ -8,7 +8,11 @@ import { Component } from '@angular/core';
 export class ADMINAccueilComponent {
   tabselector!: string;
   selected !: string;
+<<<<<<< HEAD
 
+=======
+      
+>>>>>>> 08c21cb (Màj boot projet et bases ecran compte (#32))
   ngOnInit(){
     this.tabselector='thirdTab'; //onglet par défaut, à l'ouverture
   }
@@ -25,6 +29,10 @@ export class ADMINAccueilComponent {
       } else {
         tab.classList.remove('selected');
       }
+<<<<<<< HEAD
+=======
+      
+>>>>>>> 08c21cb (Màj boot projet et bases ecran compte (#32))
     }
   }
 }
