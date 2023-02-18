342cebec9bcebfdec91bd975261740c0d54af80c
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2023-02-18 16:13:40 +0100

    ajout route et redirection fiche-compte

--- code/sono_light_reservation_angular/src/app/app-routing.module.ts
+++ code/sono_light_reservation_angular/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ADMINAccueilComponent } from './ADMIN/admin-accueil/admin-accueil.component';
+import { FicheCompteComponent } from './ADMIN/admin-comptes/fiche-compte/fiche-compte.component';
 import { AppComponent } from './app.component';
 import { CLIENTAccueilComponent } from './CLIENT/client-accueil/client-accueil.component';
 import { PageNotFoundComponent } from './GENERAL/page-not-found/page-not-found.component';
@@ -8,6 +9,7 @@ import { PageNotFoundComponent } from './GENERAL/page-not-found/page-not-found.c
 const routes: Routes = [
   { path: 'AppComponent', component: AppComponent },
   { path: 'ADMIN/admin-accueil', component: ADMINAccueilComponent },
+  { path: 'ADMIN/fiche-compte', component: FicheCompteComponent }, // Ajoutez ce chemin de route
   { path: 'CLIENT/client-accueil', component: CLIENTAccueilComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
