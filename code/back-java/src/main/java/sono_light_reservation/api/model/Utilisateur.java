7ded3c79a7382e86daa6104b8addcd80fbcbe3f5
Laurent <101107400+lolothi@users.noreply.github.com>
2023-03-08 21:19:52 +0100

    Backend : utilisateurs (#42)
    * in creation
    
    * update avec contraintes nonnull sur certaines colonnes
    
    * delete update rubrique
    
    * delete rubrique de ce push

---
+++ code/back-java/src/main/java/sono_light_reservation/api/model/Utilisateur.java
@@ -0,0 +1,40 @@
+package sono_light_reservation.api.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@Table(name= "rubrique")
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class Utilisateur {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "prenom", nullable=false)
+    private String prenom;
+
+    @Column(name = "nom", nullable=false)
+    private String nom;
+
+    @Column(nullable=false, unique=true)
+    private String email;
+
+    @Column(name = "telephone", unique=true, nullable=false)
+    private String telephone;
+
+    @Column(name = "email_verification")
+    private Boolean mail_verification;
+
+    @Column(name = "motdepasse")
+    private String motdepasse;
+
+    @Column(name = "niveau", nullable=false)
+    private Integer niveau;
+
+
+}
