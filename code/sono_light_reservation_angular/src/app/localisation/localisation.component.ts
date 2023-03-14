701e90769f7182aecb8ed407e273d4da3b2c796f
kouaouci <karimakouaouci2803@gmail.com>
2023-03-14 09:33:07 +0100

    parti localisation terminé

---
+++ code/sono_light_reservation_angular/src/app/localisation/localisation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-localisation',
+  templateUrl: './localisation.component.html',
+  styleUrls: ['./localisation.component.scss']
+})
+export class LocalisationComponent {
+
+}
