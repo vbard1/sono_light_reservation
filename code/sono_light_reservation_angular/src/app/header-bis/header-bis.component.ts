9081720c3e27e5da81c634b83f2b1c3a2783ddde
kouaouci <karimakouaouci2803@gmail.com>
2023-03-15 13:59:16 +0100

    header terminé

--- code/sono_light_reservation_angular/src/app/header-bis/header-bis.component.ts
+++ code/sono_light_reservation_angular/src/app/header-bis/header-bis.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { Link } from '../shared/UTILS/links';
 
 @Component({
   selector: 'app-header-bis',
   templateUrl: './header-bis.component.html',
-  styleUrls: ['./header-bis.component.scss']
+  styleUrls: ['./header-bis.component.scss'],
 })
 export class HeaderBisComponent {
-
+  links: Link[];
+  constructor() {
+    this.links = [];
+  }
+  ngOnInit(): void {
+    this.links.push(
+      new Link('Home', '/home'),
+      new Link('Contact', '/contact'),
+      new Link('Se Connecter/enregistrer', '/se connecter/enregistrer'),
+      new Link('Nos Materials', '/nos materials')
+    );
+  }
 }
