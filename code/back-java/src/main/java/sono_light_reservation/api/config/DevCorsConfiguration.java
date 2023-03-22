27d515af920622949bea61557e17785b20809f27
Laurent Thillou <badboboc69@gmail.com>
2023-03-22 23:39:10 +0100

    updates

---
+++ code/back-java/src/main/java/sono_light_reservation/api/config/DevCorsConfiguration.java
@@ -0,0 +1,16 @@
+package sono_light_reservation.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class DevCorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("localhost:9000/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS");
+    }
+}
