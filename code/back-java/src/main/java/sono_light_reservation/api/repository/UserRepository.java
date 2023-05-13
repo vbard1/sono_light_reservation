e8a9ae72c681a0aadf4e78403f4074dd1e914b53
Laurent <101107400+lolothi@users.noreply.github.com>
2023-05-13 20:51:14 +0200

    update for french to english, Long id to int id (#62)

--- code/back-java/src/main/java/sono_light_reservation/api/repository/UserRepository.java
+++ code/back-java/src/main/java/sono_light_reservation/api/repository/UserRepository.java
@@ -2,9 +2,9 @@ package sono_light_reservation.api.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import sono_light_reservation.api.entity.Utilisateur;
+import sono_light_reservation.api.entity.User;
 
 @Repository
-public interface UtilisateurRepository extends CrudRepository<Utilisateur, Long> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
 }
