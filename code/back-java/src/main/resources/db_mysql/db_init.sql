fb07e7aab90d4936fcd2dfe77243da7f1836554c
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2023-05-15 20:25:34 +0200

    supression des contraintes null

--- code/back-java/src/main/resources/db_mysql/db_init.sql
+++ code/back-java/src/main/resources/db_mysql/db_init.sql
@@ -1,23 +1,23 @@
 -- sonoLightReservation.`section` definition
 
 CREATE TABLE `section` (
-  `section_id` int NOT NULL AUTO_INCREMENT,
-  `label` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `section_id` int  AUTO_INCREMENT,
+  `label` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
   PRIMARY KEY (`section_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- sonoLightReservation.`user` definition
 
 CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT COMMENT 'auto increment',
-  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `phone` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_id` int  AUTO_INCREMENT COMMENT 'auto increment',
+  `firstname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
+  `email` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
+  `phone` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
   `address` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `email_validation` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT '0' COMMENT 'O validé, N non',
-  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL COMMENT 'hash uniquement',
-  `level` int NOT NULL DEFAULT '3' COMMENT '1=superAdmin, 2=admin, 3=user',
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci  COMMENT 'hash uniquement',
+  `level` int  DEFAULT '3' COMMENT '1=superAdmin, 2=admin, 3=user',
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -25,8 +25,8 @@ CREATE TABLE `user` (
 -- sonoLightReservation.category definition
 
 CREATE TABLE `category` (
-  `category_id` int NOT NULL AUTO_INCREMENT,
-  `label` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `category_id` int  AUTO_INCREMENT,
+  `label` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
   `description` varchar(1000) DEFAULT NULL,
   `picture_link` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `section_id` int DEFAULT NULL,
@@ -39,8 +39,8 @@ CREATE TABLE `category` (
 -- sonoLightReservation.equipment definition
 
 CREATE TABLE `equipment` (
-  `equipement_id` int NOT NULL AUTO_INCREMENT,
-  `label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `equipement_id` int  AUTO_INCREMENT,
+  `label` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
   `model` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `reference` varchar(255) DEFAULT NULL,
   `owner` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'KLS/CVA',
@@ -50,7 +50,7 @@ CREATE TABLE `equipment` (
   `date_buy` date DEFAULT NULL,
   `dayly_price` decimal(10,2) DEFAULT NULL,
   `replacement_price` decimal(10,2) DEFAULT NULL,
-  `category_id` int NOT NULL,
+  `category_id` int ,
   PRIMARY KEY (`equipement_id`),
   KEY `categorie_id` (`category_id`),
   CONSTRAINT `equipment_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`)
@@ -60,17 +60,17 @@ CREATE TABLE `equipment` (
 -- sonoLightReservation.event definition
 
 CREATE TABLE `event` (
-  `event_id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `event_id` int  AUTO_INCREMENT,
+  `title` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
   `location` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `type` int NOT NULL COMMENT '1 = assos insa / 2 = interne KLS / 3 = particulier/autre',
+  `type` int  COMMENT '1 = assos insa / 2 = interne KLS / 3 = particulier/autre',
   `user_comment` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `admin_comment` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `description` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `date_start` datetime DEFAULT NULL,
   `date_end` datetime DEFAULT NULL,
   `technician_asked` varchar(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT 'O / N',
-  `user_id` int NOT NULL,
+  `user_id` int ,
   PRIMARY KEY (`event_id`),
   KEY `event_FK` (`user_id`),
   CONSTRAINT `event_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
@@ -80,8 +80,8 @@ CREATE TABLE `event` (
 -- sonoLightReservation.reservation definition
 
 CREATE TABLE `reservation` (
-  `reservation_id` int NOT NULL AUTO_INCREMENT,
-  `reservation_label` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `reservation_id` int  AUTO_INCREMENT,
+  `reservation_label` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ,
   `reservation_state` int DEFAULT NULL COMMENT '(1=enAttente, 2=validée, 3=enCours, 4=terminée)',
   `equipment_id` int DEFAULT NULL,
   `category_id` int DEFAULT NULL,
