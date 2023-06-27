a52df2959d5d3f737b3e289636324cbe192d6bd9
kouaouci <karimakouaouci2803@gmail.com>
2023-06-27 10:10:50 +0200

    resoudre conflit fichier appRouting

--- code/sono_light_reservation_angular/src/app/app-routing.module.ts
+++ code/sono_light_reservation_angular/src/app/app-routing.module.ts
@@ -3,27 +3,23 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './GENERAL/page-not-found/page-not-found.component';
-<<<<<<< HEAD
-import { HomeComponent } from './home/home.component';
-=======
 import { AdminHomeComponent } from './ADMIN/admin-home/admin-home.component';
 import { AdminUserListComponent } from './ADMIN/admin-user-list/admin-user-list.component';
 import { AdminUserDetailsComponent } from './ADMIN/admin-user-details/admin-user-details.component';
 import { UserHomeComponent } from './USER/user-home/user-home.component';
 
->>>>>>> developpement
 const routes: Routes = [
   { path: 'AppComponent', component: AppComponent },
   { path: 'ADMIN/admin-accueil', component: AdminHomeComponent },
   { path: 'ADMIN/comptes', component: AdminUserListComponent },
   {
     path: 'ADMIN/fiche-compte/:user_id',
-    component: AdminUserDetailsComponent
-  }, 
+    component: AdminUserDetailsComponent,
+  },
   { path: 'CLIENT/client-accueil', component: UserHomeComponent },
   { path: '', redirectTo: '/ADMIN/comptes', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
-  { path: 'HomeComponent', component: HomeComponent },
+  { path: 'HomeComponent', component: UserHomeComponent },
 ];
 
 @NgModule({
