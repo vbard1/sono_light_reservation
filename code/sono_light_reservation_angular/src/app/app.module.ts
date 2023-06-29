8e8d99e0e2ffc052b1458ec9bd991b496692b771
kouaouci <karimakouaouci2803@gmail.com>
2023-06-29 21:49:14 +0200

    resourdre probleme importation

--- code/sono_light_reservation_angular/src/app/app.module.ts
+++ code/sono_light_reservation_angular/src/app/app.module.ts
@@ -1,8 +1,3 @@
-
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-
 import { MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
 
 import { AdminEquipmentListComponent } from './ADMIN/admin-equipment-list/admin-equipment-list.component';
@@ -10,36 +5,22 @@ import { AdminHomeComponent } from './ADMIN/admin-home/admin-home.component';
 import { AdminReservationComponent } from './ADMIN/admin-reservation/admin-reservation.component';
 import { AdminUserDetailsComponent } from './ADMIN/admin-user-details/admin-user-details.component';
 import { AdminUserListComponent } from './ADMIN/admin-user-list/admin-user-list.component';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PageNotFoundComponent } from './GENERAL/page-not-found/page-not-found.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { AdminHomeComponent } from './ADMIN/admin-home/admin-home.component';
-import { AdminEquipmentListComponent } from './ADMIN/admin-equipment-list/admin-equipment-list.component';
-import { AdminReservationComponent } from './ADMIN/admin-reservation/admin-reservation.component';
-import { AdminUserListComponent } from './ADMIN/admin-user-list/admin-user-list.component';
-import { AdminUserDetailsComponent } from './ADMIN/admin-user-details/admin-user-details.component';
-
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './GENERAL/page-not-found/page-not-found.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { UserHomeComponent } from './USER/user-home/user-home.component';
+import { CarousselComponent } from './caroussel/caroussel.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -51,6 +32,8 @@ import { UserHomeComponent } from './USER/user-home/user-home.component';
     AdminReservationComponent,
     AdminUserDetailsComponent,
     UserHomeComponent,
+    CarousselComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
